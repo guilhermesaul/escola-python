adf5cdadccfb55e3c7d9e48b41122844d95a9796
guisaul <guilhermesaul190@gmail.com>
2025-06-26 22:19:36 -0300

    arquivos novos

---
+++ conteúdo/POO/atividade 03/ex001.py
@@ -0,0 +1,503 @@
+class KazuyaMishima:
+    def __init__(self, nome, idade, sexo, ataque, defesa, vida):
+        self.__nome = nome
+        self.__idade = idade
+        self.__sexo = sexo
+        self.__ataque = ataque
+        self.__defesa = defesa
+        self.__vida = vida
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @property
+    def idade(self):
+        return self.__idade
+
+    @property
+    def sexo(self):
+        return self.__sexo
+    
+    @property
+    def ataque(self):
+        return self.__ataque
+
+    @property
+    def defesa(self):
+        return self.__defesa
+
+    @property
+    def vida(self):
+        return self.__vida
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome
+
+    @idade.setter
+    def idade(self, nova_idade):
+        self.__idade = nova_idade
+        
+    @sexo.setter
+    def sexo(self, novo_sexo):
+        self.__sexo = novo_sexo
+        
+    @ataque.setter
+    def ataque(self, novo_ataque):
+        self.__ataque = novo_ataque
+        
+    @defesa.setter
+    def defesa(self, nova_defesa):
+        self.__defesa = nova_defesa
+        
+    @vida.setter
+    def vida(self, nova_vida):
+        self.__vida = nova_vida
+        
+    def bloquear_golpe(self):
+        self.__defesa *= 2
+        
+    def golpe_especial(self):
+        self.__ataque *= 3
+        
+class King:
+    def __init__(self, nome, idade, sexo, ataque, defesa, vida):
+        self.__nome = nome
+        self.__idade = idade
+        self.__sexo = sexo
+        self.__ataque = ataque
+        self.__defesa = defesa
+        self.__vida = vida
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @property
+    def idade(self):
+        return self.__idade
+
+    @property
+    def sexo(self):
+        return self.__sexo
+    
+    @property
+    def ataque(self):
+        return self.__ataque
+
+    @property
+    def defesa(self):
+        return self.__defesa
+
+    @property
+    def vida(self):
+        return self.__vida
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome
+
+    @idade.setter
+    def idade(self, nova_idade):
+        self.__idade = nova_idade
+        
+    @sexo.setter
+    def sexo(self, novo_sexo):
+        self.__sexo = novo_sexo
+        
+    @ataque.setter
+    def ataque(self, novo_ataque):
+        self.__ataque = novo_ataque
+        
+    @defesa.setter
+    def defesa(self, nova_defesa):
+        self.__defesa = nova_defesa
+        
+    @vida.setter
+    def vida(self, nova_vida):
+        self.__vida = nova_vida
+        
+    def bloquear_golpe(self):
+        self.__defesa *= 2
+        
+    def golpe_especial(self):
+        self.__ataque *= 3
+        
+class NinaWilliams:
+    def __init__(self, nome, idade, sexo, ataque, defesa, vida):
+        self.__nome = nome
+        self.__idade = idade
+        self.__sexo = sexo
+        self.__ataque = ataque
+        self.__defesa = defesa
+        self.__vida = vida
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @property
+    def idade(self):
+        return self.__idade
+
+    @property
+    def sexo(self):
+        return self.__sexo
+    
+    @property
+    def ataque(self):
+        return self.__ataque
+
+    @property
+    def defesa(self):
+        return self.__defesa
+
+    @property
+    def vida(self):
+        return self.__vida
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome
+
+    @idade.setter
+    def idade(self, nova_idade):
+        self.__idade = nova_idade
+        
+    @sexo.setter
+    def sexo(self, novo_sexo):
+        self.__sexo = novo_sexo
+        
+    @ataque.setter
+    def ataque(self, novo_ataque):
+        self.__ataque = novo_ataque
+        
+    @defesa.setter
+    def defesa(self, nova_defesa):
+        self.__defesa = nova_defesa
+        
+    @vida.setter
+    def vida(self, nova_vida):
+        self.__vida = nova_vida
+        
+    def bloquear_golpe(self):
+        self.__defesa *= 2
+        
+    def golpe_especial(self):
+        self.__ataque *= 3
+        
+class PaulPhoenix :
+    def __init__(self, nome, idade, sexo, ataque, defesa, vida):
+        self.__nome = nome
+        self.__idade = idade
+        self.__sexo = sexo
+        self.__ataque = ataque
+        self.__defesa = defesa
+        self.__vida = vida
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @property
+    def idade(self):
+        return self.__idade
+
+    @property
+    def sexo(self):
+        return self.__sexo
+    
+    @property
+    def ataque(self):
+        return self.__ataque
+
+    @property
+    def defesa(self):
+        return self.__defesa
+
+    @property
+    def vida(self):
+        return self.__vida
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome
+
+    @idade.setter
+    def idade(self, nova_idade):
+        self.__idade = nova_idade
+        
+    @sexo.setter
+    def sexo(self, novo_sexo):
+        self.__sexo = novo_sexo
+        
+    @ataque.setter
+    def ataque(self, novo_ataque):
+        self.__ataque = novo_ataque
+        
+    @defesa.setter
+    def defesa(self, nova_defesa):
+        self.__defesa = nova_defesa
+        
+    @vida.setter
+    def vida(self, nova_vida):
+        self.__vida = nova_vida
+        
+    def bloquear_golpe(self):
+        self.__defesa *= 2
+        
+    def golpe_especial(self):
+        self.__ataque *= 3
+        
+class MichelleChang:
+    def __init__(self, nome, idade, sexo, ataque, defesa, vida):
+        self.__nome = nome
+        self.__idade = idade
+        self.__sexo = sexo
+        self.__ataque = ataque
+        self.__defesa = defesa
+        self.__vida = vida
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @property
+    def idade(self):
+        return self.__idade
+
+    @property
+    def sexo(self):
+        return self.__sexo
+    
+    @property
+    def ataque(self):
+        return self.__ataque
+
+    @property
+    def defesa(self):
+        return self.__defesa
+
+    @property
+    def vida(self):
+        return self.__vida
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome
+
+    @idade.setter
+    def idade(self, nova_idade):
+        self.__idade = nova_idade
+        
+    @sexo.setter
+    def sexo(self, novo_sexo):
+        self.__sexo = novo_sexo
+        
+    @ataque.setter
+    def ataque(self, novo_ataque):
+        self.__ataque = novo_ataque
+        
+    @defesa.setter
+    def defesa(self, nova_defesa):
+        self.__defesa = nova_defesa
+        
+    @vida.setter
+    def vida(self, nova_vida):
+        self.__vida = nova_vida
+        
+    def bloquear_golpe(self):
+        self.__defesa *= 2
+        
+    def golpe_especial(self):
+        self.__ataque *= 3
+        
+class Law:
+    def __init__(self, nome, idade, sexo, ataque, defesa, vida):
+        self.__nome = nome
+        self.__idade = idade
+        self.__sexo = sexo
+        self.__ataque = ataque
+        self.__defesa = defesa
+        self.__vida = vida
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @property
+    def idade(self):
+        return self.__idade
+
+    @property
+    def sexo(self):
+        return self.__sexo
+    
+    @property
+    def ataque(self):
+        return self.__ataque
+
+    @property
+    def defesa(self):
+        return self.__defesa
+
+    @property
+    def vida(self):
+        return self.__vida
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome
+
+    @idade.setter
+    def idade(self, nova_idade):
+        self.__idade = nova_idade
+        
+    @sexo.setter
+    def sexo(self, novo_sexo):
+        self.__sexo = novo_sexo
+        
+    @ataque.setter
+    def ataque(self, novo_ataque):
+        self.__ataque = novo_ataque
+        
+    @defesa.setter
+    def defesa(self, nova_defesa):
+        self.__defesa = nova_defesa
+        
+    @vida.setter
+    def vida(self, nova_vida):
+        self.__vida = nova_vida
+        
+    def bloquear_golpe(self):
+        self.__defesa *= 2
+        
+    def golpe_especial(self):
+        self.__ataque *= 3
+        
+class Yoshimitsu:
+    def __init__(self, nome, idade, sexo, ataque, defesa, vida):
+        self.__nome = nome
+        self.__idade = idade
+        self.__sexo = sexo
+        self.__ataque = ataque
+        self.__defesa = defesa
+        self.__vida = vida
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @property
+    def idade(self):
+        return self.__idade
+
+    @property
+    def sexo(self):
+        return self.__sexo
+    
+    @property
+    def ataque(self):
+        return self.__ataque
+
+    @property
+    def defesa(self):
+        return self.__defesa
+
+    @property
+    def vida(self):
+        return self.__vida
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome
+
+    @idade.setter
+    def idade(self, nova_idade):
+        self.__idade = nova_idade
+        
+    @sexo.setter
+    def sexo(self, novo_sexo):
+        self.__sexo = novo_sexo
+        
+    @ataque.setter
+    def ataque(self, novo_ataque):
+        self.__ataque = novo_ataque
+        
+    @defesa.setter
+    def defesa(self, nova_defesa):
+        self.__defesa = nova_defesa
+        
+    @vida.setter
+    def vida(self, nova_vida):
+        self.__vida = nova_vida
+        
+    def bloquear_golpe(self):
+        self.__defesa *= 2
+        
+    def golpe_especial(self):
+        self.__ataque *= 3
+        
+class Jack:
+    def __init__(self, nome, idade, sexo, ataque, defesa, vida):
+        self.__nome = nome
+        self.__idade = idade
+        self.__sexo = sexo
+        self.__ataque = ataque
+        self.__defesa = defesa
+        self.__vida = vida
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @property
+    def idade(self):
+        return self.__idade
+
+    @property
+    def sexo(self):
+        return self.__sexo
+    
+    @property
+    def ataque(self):
+        return self.__ataque
+
+    @property
+    def defesa(self):
+        return self.__defesa
+
+    @property
+    def vida(self):
+        return self.__vida
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome
+
+    @idade.setter
+    def idade(self, nova_idade):
+        self.__idade = nova_idade
+        
+    @sexo.setter
+    def sexo(self, novo_sexo):
+        self.__sexo = novo_sexo
+        
+    @ataque.setter
+    def ataque(self, novo_ataque):
+        self.__ataque = novo_ataque
+        
+    @defesa.setter
+    def defesa(self, nova_defesa):
+        self.__defesa = nova_defesa
+        
+    @vida.setter
+    def vida(self, nova_vida):
+        self.__vida = nova_vida
+        
+    def bloquear_golpe(self):
+        self.__defesa *= 2
+        
+    def golpe_especial(self):
+        self.__ataque *= 3
